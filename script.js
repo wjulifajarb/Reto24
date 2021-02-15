349eee55be6f0d936c518554af39e304eecfaa3c
Juliana Fajardo <juliana.fajardo92@gmail.com>
2021-02-15 16:51:16 -0500

    agregarvar

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const ratings = document.querySelectorAll('.raing')
+const ratings = document.querySelectorAll('.rating')
 const ratingsContainer = document.querySelector('.ratings-container')
-const sendBtn = document.querySelector('#send') /*completar*/
+const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
-
+let selectedRating = 'Satisfied'
 
 ratingsContainer.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains('rating')) {
@@ -14,18 +14,16 @@ ratingsContainer.addEventListener('click', (e) => {
 
 sendBtn.addEventListener('click', (e) => {
     panel.innerHTML = `
-    <i class="fas fa-heart"></i>
-    <strong>Thank You!</strong>
-    <br>
-    <strong>Feedback: ${selectedRating}</strong>
-    <p>We'll use your feedback to improve our customer support</p>
-
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support</p>
     `
 })
 
 function removeActive() {
-{
+    for(let i = 0; i < ratings.length; i++) {
         ratings[i].classList.remove('active')
     }
 }
-/*quitar css raro*/
